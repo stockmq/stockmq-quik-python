cfb59d43689a5a5b0c63eb4810610854fa42e406
Alexander Nusov <nusov@users.noreply.github.com>
2024-02-20 18:43:37 +0300

    move tx

--- stockmq/api.py
+++ stockmq/api.py
@@ -7,61 +7,9 @@ from typing import Any
 
 from stockmq.ns.info import QuikInfo
 from stockmq.ns.lua import QuikLua
+from stockmq.ns.tx import QuikTx
 from stockmq.rpc import RPCClient
 from stockmq.ns.table import QuikTable
-
-
-class TxTimeoutError(Exception):
-    pass
-
-
-class TxRejectedError(Exception):
-    pass
-
-
-
-class Action(str, Enum):
-    NEW_ORDER = "NEW_ORDER"
-    NEW_STOP_ORDER = "NEW_STOP_ORDER"
-    KILL_ORDER = "KILL_ORDER"
-    KILL_STOP_ORDER = "KILL_STOP_ORDER"
-
-
-class OrderType(str, Enum):
-    LIMIT = 'L'
-    MARKET = 'M'
-
-class TimeInForce(str, Enum):
-    FOK = "FILL_OR_KILL"
-    IOC = "KILL_BALANCE"
-    DAY = "PUT_IN_QUEUE"
-
-
-class Side(str, Enum):
-    BUY = 'B'
-    SELL = 'S'
-
-
-class TransactionState(str, Enum):
-    ACCEPTED = "ACCEPTED"
-    REJECTED = "REJECTED"
-    EXECUTED = "EXECUTED"
-
-
-class Transaction(BaseModel):
-    id: int
-    action: Action
-    board: str
-    order_id: int
-    created_ts: float
-    updated_ts: float
-    state: TransactionState
-    message: str
-
-class Order(BaseModel):
-    id: int
-    board: str
-
 
 
 
@@ -70,8 +18,6 @@ class Order(BaseModel):
     
 
 class Quik(RPCClient):
-    TX_SLEEP_TIMEOUT = 0.01
-
     @property
     def lua(self) -> QuikLua:
         return QuikLua(self)
@@ -203,44 +149,7 @@ class Quik(RPCClient):
     def get_security_info(self, class_name, sec_name) -> Any:
         return self.call("getSecurityInfo", class_name, sec_name)
 
-    async def wait_tx(self, tx: Transaction, timeout=1.0) -> Transaction:
-        t0 = time.time()
-        while True:
-            if time.time() - t0 >= timeout:
-                raise TxTimeoutError()
-            elif tx.state == TransactionState.EXECUTED:
-                return tx
-            elif tx.state == TransactionState.REJECTED:
-                print(tx)
-                raise TxRejectedError(tx.message)
-            elif tx.state == TransactionState.ACCEPTED:
-                await asyncio.sleep(self.TX_SLEEP_TIMEOUT)
-                tx = self.update_transaction(tx)
-                print(tx)
-
-    def create_order_tx(self, account: str, client: str, board: str, ticker: str, tif: TimeInForce, side: Side, price: float, quantity: int) -> Transaction:
-        return Transaction(**self.call("stockmq_create_order", account, client, board, ticker, tif.value, side.value, price, quantity))
-
-    def create_stop_order_tx(self, account: str, client: str, board: str, ticker: str, tif: TimeInForce, side: Side, price: float, stop_price: float, quantity: int) -> Transaction:
-        return Transaction(**self.call("stockmq_create_simple_stop_order", account, client, board, ticker, tif.value, side.value, price, stop_price, quantity))
-
-    def cancel_order_tx(self, account: str, client: str, board: str, ticker: str, order_id: int) -> Transaction:
-        return Transaction(**self.call("stockmq_cancel_order", account, client, board, ticker, order_id))
-
-    def cancel_stop_order_tx(self, account: str, client: str, board: str, ticker: str, order_id: int) -> Transaction:
-        return Transaction(**self.call("stockmq_cancel_stop_order", account, client, board, ticker, order_id))
-
-    def update_transaction(self, tx: Transaction) -> Transaction:
-        return Transaction(**self.call("stockmq_update_tx", tx.dict()))
-
-    async def create_order(self, account: str, client: str, board: str, ticker: str, tif: TimeInForce, side: Side, price: float, quantity: int, timeout: float = 1.0) -> Transaction:
-        return await self.wait_tx(self.create_order_tx(account, client, board, ticker, tif, side, price, quantity), timeout)
-
-    async def create_stop_order(self, account: str, client: str, board: str, ticker: str, tif: TimeInForce, side: Side, price: float, stop_price: float, quantity: int, timeout: float = 1.0) -> Transaction:
-        return await self.wait_tx(self.create_stop_order_tx(account, client, board, ticker, tif, side, price, stop_price, quantity), timeout)
-
-    async def cancel_order(self, account: str, client: str, board: str, ticker: str, order_id: int, timeout: float = 1.0) -> Transaction:
-        return await self.wait_tx(self.cancel_order_tx(account, client, board, ticker, order_id), timeout)
-
-    async def cancel_stop_order(self, account: str, client: str, board: str, ticker: str, order_id: int, timeout: float = 1.0) -> Transaction:
-        return await self.wait_tx(self.cancel_stop_order_tx(account, client, board, ticker, order_id), timeout)
+    @property
+    def tx(self) -> QuikTx:
+        """Returns Transaction manager"""
+        return QuikTx(self)
--- tx.py
+++ tx.py
@@ -2,7 +2,8 @@
 import asyncio
 import time
 
-from stockmq.api import Quik, TimeInForce, Side, QuikTable
+from stockmq.api import Quik
+from stockmq.ns.tx import TimeInForce, Side
 
 api = Quik("tcp://10.211.55.3:8004")
 
@@ -16,24 +17,18 @@ async def main():
         print(f"Quik version: {api.info.VERSION}")
         print(f"Is Connected: {api.is_connected}")
 
-        for i in api.client_codes:
-            print(i)
-
-        t0 = time.time()
-
         # Create transaction to BUY and wait for completion
-        tx = await api.create_order(account, client, board, ticker, TimeInForce.DAY, Side.BUY, 265, 1)
+        t0 = time.time()
+        tx = await api.tx.create_order(account, client, board, ticker, TimeInForce.DAY, Side.BUY, 265, 1)
         print(tx)
         print(tx.updated_ts - tx.created_ts)
 
         # Create transaction to cancel the order
-        tx = await api.cancel_order(account, client, board, ticker, tx.order_id, timeout=4.0)
+        tx = await api.tx.cancel_order(account, client, board, ticker, tx.order_id, timeout=4.0)
         print(tx)
         print(tx.updated_ts - tx.created_ts)
 
         print(f"Time to create and cancel: {time.time()-t0}")
-
-
 
 if __name__ == '__main__':
     asyncio.run(main())
